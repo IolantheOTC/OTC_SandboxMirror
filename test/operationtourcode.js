3631f436b821e3aa067665465452ec8ae8170e90
WeWuzNidokangz <43294696+WeWuzNidokangz@users.noreply.github.com>
2023-12-31 17:29:22 +0900

    Test with a failing challenge code

--- test/operationtourcode.js
+++ test/operationtourcode.js
@@ -8,8 +8,8 @@ describe(`OperationTourCode`, function () {
 		try {
 			const tryFormat = Dex.formats.validate(
 				// Example challenge codes:
-				`[Gen 9] Random Battle@@@Chimera 1v1 Rule,Max Team Size=24,Adjust Level=100,Terastal Clause`
-				//`[Gen 9] Random Doubles Battle@@@Chimera 1v1 Rule,Max Team Size=24,Adjust Level=100,Terastal Clause`
+				//`[Gen 9] Random Battle@@@Chimera 1v1 Rule,Max Team Size=24,Adjust Level=100,Terastal Clause`
+				`[Gen 9] Random Doubles Battle@@@Chimera 1v1 Rule,Max Team Size=24,Adjust Level=100,Terastal Clause`
 			);
 			const format = Dex.formats.get(tryFormat, true);
 		} catch (err) {
